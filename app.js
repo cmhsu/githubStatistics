6a1564f8bad0463aeb72bcd96bd69d6e599e0e13
Corey Hsu <corey81@gmail.com>
2015-10-14 00:54:03 -0700

    Cleaned up code and refactored.

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var app = function() {
   Chart.defaults.global.showTooltips = false;
-  var displayNumberOne = 10;
-  var displayNumberThree = 5;
-  var displayNumberFour = 10;
-  var displayNumberFive = 10;
+  var displayNumbers = {
+    one: 10,
+    three: 5,
+    four: 10,
+    five: 10
+  };
+  var times = {
+    one: '>2015-09-12',
+    two: '>2015-09-12',
+    five: '>2015-09-12'
+  };
   var languageThird = 'ruby';
   var languageFourth = 'ruby';
   var queries = [
-    'https://api.github.com/search/repositories?q=created:' + time + '&sort=stars&per_page=100&page=1',
+    'https://api.github.com/search/repositories?q=created:' + times.one + '&sort=stars&per_page=100&page=1',
     'https://api.github.com/search/repositories?q=language:' + languageThird + '&sort=stars&per_page=100&page=1',
     'https://api.github.com/search/users?q=language:' + languageFourth + '&sort=followers&per_page=100&page=1',
-    'https://api.github.com/search/repositories?q=created:' + timeFive + '&sort=forks&per_page=100&page=1'
+    'https://api.github.com/search/repositories?q=created:' + times.five + '&sort=forks&per_page=100&page=1'
   ];
   var queryResult;
   var chartOne;
@@ -18,130 +25,60 @@ var app = function() {
   var chartThree;
   var chartFour;
   var chartFive;
-  var chartOneCache = {};
-  var chartTwoCache = {};
-  var chartThreeCache = {};
-  var chartFourCache = {};
-  var chartFiveCache = {};
-  var oneCurrentValues;
-  var twoCurrentValues;
-  var threeCurrentValues;
-  var fourCurrentValues;
-  var fiveCurrentValues;
-  var time = '>2015-09-12';
-  var timeTwo = '>2015-09-12';
-  var timeFive = '>2015-09-12';
+  var cache = {};
+  var currentValues = {};
 
   function makeRequest(query, chartNumber, queryParam) {
-    if (chartNumber === 'one') {
-      //check if we have the cached value of the request already
-      if (chartOneCache[queryParam]) {
-        oneCurrentValues = chartOneCache[queryParam];
-        displayChartOneThreeFourFive(chartOneCache[queryParam].items.slice(0, displayNumberOne), 'one');
+    if (cache[chartNumber] && cache[chartNumber][queryParam]) {
+      currentValues[chartNumber] = cache[chartNumber][queryParam];
+      if (chartNumber === 'two') {
+        displayChartTwo(currentValues[chartNumber].items);
+        console.log(currentValues[chartNumber].items);
         return;
-      }
-    } else if (chartNumber === 'two') {
-      if (chartTwoCache[queryParam]) {
-        twoCurrentValues = chartTwoCache[queryParam];
-        displayChartTwo(chartTwoCache[queryParam].items);
-        return;
-      }
-    } else if (chartNumber === 'three') {
-      if (chartThreeCache[queryParam]) {
-        threeCurrentValues = chartThreeCache[queryParam];
-        displayChartOneThreeFourFive(chartThreeCache[queryParam].items.slice(0, displayNumberThree), 'three');
-        return;
-      }
-    } else if (chartNumber === 'four') {
-      if (chartFourCache[queryParam]) {
-        fourCurrentValues = chartFourCache[queryParam];
-        displayChartOneThreeFourFive(chartFourCache[queryParam].items.slice(0, displayNumberFour), 'four');
-        return;
-      }
-    } else if (chartNumber === 'five') {
-      if (chartFiveCache[queryParam]) {
-        fiveCurrentValues = chartFiveCache[queryParam];
-        displayChartOneThreeFourFive(chartFiveCache[queryParam].items.slice(0, displayNumberFive), 'five');
+      } else {
+        displayChartOneThreeFourFive(currentValues[chartNumber].items.slice(0, displayNumbers[chartNumber]), chartNumber);
         return;
       }
     }
-    var requestOne = new XMLHttpRequest();
-    requestOne.onreadystatechange = function () {
-      if (requestOne.readyState == 4 && requestOne.status == 200) {
-        queryResult = JSON.parse(requestOne.responseText);
-        if (chartNumber === 'one') {
-          //cache the result so we only do each unique request once.
-          chartOneCache[queryParam] = queryResult;
-          oneCurrentValues = queryResult;
-          displayChartOneThreeFourFive(queryResult.items.slice(0, displayNumberOne), 'one');
-        } else if (chartNumber === 'two') {
-          chartTwoCache[queryParam] = queryResult;
-          twoCurrentValues = queryResult;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == 200) {
+        queryResult = JSON.parse(request.responseText);
+        //cache the result so we only do each unique request once.
+        if (!cache[chartNumber]) {
+          cache[chartNumber] = {};
+        }
+        cache[chartNumber][queryParam] = queryResult;
+        currentValues[chartNumber] = queryResult;
+        if (chartNumber === 'two') {
           displayChartTwo(queryResult.items);
-        } else if (chartNumber === 'three') {
-          chartThreeCache[queryParam] = queryResult;
-          threeCurrentValues = queryResult;
-          displayChartOneThreeFourFive(queryResult.items.slice(0, displayNumberThree), 'three');
-        } else if (chartNumber === 'four') {
-          chartFourCache[queryParam] = queryResult;
-          fourCurrentValues = queryResult;
-          displayChartOneThreeFourFive(queryResult.items.slice(0, displayNumberFour), 'four');
-        } else if (chartNumber === 'five') {
-          chartFiveCache[queryParam] = queryResult;
-          fiveCurrentValues = queryResult;
-          displayChartOneThreeFourFive(queryResult.items.slice(0, displayNumberFive), 'five');
+        } else {
+          displayChartOneThreeFourFive(queryResult.items.slice(0, displayNumbers[chartNumber]), chartNumber);
         }
       }
     };
-    requestOne.open('GET', query);
-    requestOne.send();
+    request.open('GET', query);
+    request.send();
   }
 
-  function changeTime(value, className) {
-    if (className === 'selectFirst') {
-      if (value === 'month') {
-        time = '>2015-09-12';
-      } else if (value === 'sixMonths') {
-        time = '>2015-04-12'
-      } else if (value === 'year') {
-        time = '>2014-10-12'
-      }
-      makeRequest('https://api.github.com/search/repositories?q=a+created:' + time + '&sort=stars&per_page=100', 'one', time);
-    } else if (className === 'selectSecond') {
-      if (value === 'month') {
-        timeTwo = '>2015-09-12';
-      } else if (value === 'sixMonths') {
-        timeTwo = '>2015-04-12'
-      } else if (value === 'year') {
-        timeTwo = '>2014-10-12'
-      }
-      makeRequest('https://api.github.com/search/repositories?q=a+created:' + timeTwo + '&sort=stars&per_page=100', 'two', timeTwo);
-    } else if (className === 'selectFifth') {
-      if (value === 'month') {
-        timeFive = '>2015-09-12';
-      } else if (value === 'sixMonths') {
-        timeFive = '>2015-04-12'
-      } else if (value === 'year') {
-        timeFive = '>2014-10-12'
-      }
-      makeRequest('https://api.github.com/search/repositories?q=created:' + timeFive + '&sort=forks&per_page=100', 'five', timeFive);
+  function changeTime(value, chartNumber) {
+    if (value === 'month') {
+      times[chartNumber] = '>2015-09-12';
+    } else if (value === 'sixMonths') {
+      times[chartNumber] = '>2015-04-12';
+    } else if (value === 'year') {
+      times[chartNumber] = '>2014-10-12';
+    }
+    if (chartNumber === 'five') {
+      makeRequest('https://api.github.com/search/repositories?q=created:' + times[chartNumber] + '&sort=forks&per_page=100&page=1', 'five', times[chartNumber]);
+    } else if (chartNumber === 'one' || chartNumber === 'two') {
+      makeRequest('https://api.github.com/search/repositories?q=created:' + times[chartNumber] + '&sort=stars&per_page=100&page=1', chartNumber, times[chartNumber]);
     }
   }
 
-  function changeNumber(value, className) {
-    if (className === 'firstInput') {
-      displayNumberOne = value;
-      displayChartOneThreeFourFive(oneCurrentValues.items.slice(0, displayNumberOne), 'one');
-    } else if (className === 'thirdInput') {
-      displayNumberThree = value;
-      displayChartOneThreeFourFive(threeCurrentValues.items.slice(0, displayNumberThree), 'three');
-    } else if (className === 'fourthInput') {
-      displayNumberFour = value;
-      displayChartOneThreeFourFive(fourCurrentValues.items.slice(0, displayNumberFour), 'four');
-    } else if (className === 'fifthInput') {
-      displayNumberFive = value;
-      displayChartOneThreeFourFive(fiveCurrentValues.items.slice(0, displayNumberFive), 'five');
-    }
+  function changeNumber(value, chartNumber) {
+    displayNumbers[chartNumber] = value;
+    displayChartOneThreeFourFive(currentValues[chartNumber].items.slice(0, value), chartNumber);
   }
 
   function displayChartOneThreeFourFive(info, chartNumber) {
@@ -154,15 +91,7 @@ var app = function() {
     } else if (chartFive && chartNumber === 'five') {
       chartFive.destroy();
     }
-    if (chartNumber === 'one') {
-      var ctx = document.getElementById('chartOne').getContext('2d');
-    } else if (chartNumber === 'three') {
-      var ctx = document.getElementById('chartThree').getContext('2d');
-    } else if (chartNumber === 'four') {
-      var ctx = document.getElementById('chartFour').getContext('2d');
-    } else if (chartNumber === 'five') {
-      var ctx = document.getElementById('chartFive').getContext('2d');
-    }
+    var ctx = document.getElementById(chartNumber).getContext('2d');
     var labels = [];
     var data = [];
     if (chartNumber === 'one' || chartNumber === 'three') {
@@ -270,7 +199,7 @@ var app = function() {
       } else if (value === 'objective-c') {
         languageThird = 'objective-c';
       }
-      makeRequest('https://api.github.com/search/repositories?q=a+language:' + languageThird + '&sort=stars&per_page=100&page=1', 'three', languageThird)
+      makeRequest('https://api.github.com/search/repositories?q=language:' + languageThird + '&sort=stars&per_page=100&page=1', 'three', languageThird)
     } else if (className === 'selectFourth') {
       if (value === 'ruby') {
         languageFourth = 'ruby';
@@ -285,11 +214,11 @@ var app = function() {
     }
   }
 
-  makeRequest(queries[0], 'one', time);
-  makeRequest(queries[0], 'two', timeTwo);
+  makeRequest(queries[0], 'one', times.one);
+  makeRequest(queries[0], 'two', times.two);
   makeRequest(queries[1], 'three', languageThird);
   makeRequest(queries[2], 'four', languageFourth);
-  makeRequest(queries[3], 'five', timeFive);
+  makeRequest(queries[3], 'five', times.five);
 
   return { //expose methods to global scope.
     changeTime: changeTime,
